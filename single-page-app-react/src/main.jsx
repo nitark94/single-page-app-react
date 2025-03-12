782304fb209b94a4cbcbcf60460b0c025a957a90
nitark94 <nita.rantaknuuttila@gmail.com>
2025-03-12 12:01:00 +0200

    Tehty Refaktorointia - moduulit kohta valmiiksi

--- single-page-app-react/src/main.jsx
+++ single-page-app-react/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const notes = [
+  { id: 1, content: "HTML is easy" },
+  { id: 2, content: "Browser can execute only JavaScript" },
+  { id: 3, content: "GET and POST are the most important methods of HTTP protocol" }
+];
+
+const result = notes.map(note => note.content)
+console.log(result)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <App notes={notes} />
+);
