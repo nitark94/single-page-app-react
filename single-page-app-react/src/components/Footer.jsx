323877a1e2b8b3ae058cf79a789f277a04dc381e
nitark94 <nita.rantaknuuttila@gmail.com>
2025-05-15 10:47:36 +0300

    päivitetty ajantasalle, bugit korjattu, kansioiden sisältöä muutettu että toimii

--- single-page-app-react/src/components/Footer.jsx
+++ single-page-app-react/src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
     <div style={footerStyle}>
       <br />
       <p>
-        Note app, Department of Computer Science, University of Helsinki 2025
+        Created by Nita :)
       </p>
     </div>
   )
